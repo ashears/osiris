2a7505a0729865889fd94d2dd2bf44f3c5c4bec8
Ashton Shears <shears.ash@gmail.com>
2019-10-19 19:33:03 -0700

    started on dynamic routers

--- onlinesheets/app.py
+++ onlinesheets/app.py
@@ -1,11 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from typing import List
 from pydantic import BaseModel
 
-class OnlineSheets():
+
+class Table(BaseModel):
+    table_name: str
+    table: BaseModel
+
+
+
+def process_tables(tables: List[BaseModel]):
+    for table in tables:
+        create_router(table)
+        pass
+        # Create the fastapi portion
+        # 1. add
+
+
+class OnlineSheets:
     def __init__(self,
                  tables: List[BaseModel]):
         self.app = FastAPI()
-        self.db
+        # self.db
+
+        process_tables(tables)
+
+    def create_router(self, table: Table):
+        router = APIRouter()
+
+        @router.get("/")
+        def get_table():
+            pass
+
+        @router.post("/")
+        def insert_entry():
+            pass
+
+        @router.delete("/{id}")
+        def delete_entry_by_id(id: int):
+            pass
+
+        # TODO: Try to find a way to make a dynamic query args get
+
+        self.app.include_router(router, prefix=table.table_name)
 
     pass
